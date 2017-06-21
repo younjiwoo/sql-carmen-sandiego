92fbed1ed2e96bdf4184c39acb99a9f1dad7f502
Nathan Allen <thenathanator@gmail.com>
2017-06-21 07:11:07 -0700

    fixes #3 - combine 7/8

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -37,10 +37,8 @@
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
- -- landing dock.
-
--- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
--- if we can crack it, we can finally put her where she belongs – behind bars.
+ -- landing dock. Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
+ -- if we can crack it, we can finally put her where she belongs – behind bars.
 
 -- Our playdate of late has been unusually fun –
 -- As an agent, I'll say, you've been a joy to outrun.
