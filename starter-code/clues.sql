e0ec87bc101903319a8a5b4bedc47c6b4433262e
Nathan Allen <thenathanator@gmail.com>
2017-06-21 07:11:58 -0700

    formatting

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,18 +1,18 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
--- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
--- so find the least populated country in Southern Europe, and we'll start looking for her there.
+ -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
+ -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
--- this country's officially recognized language. Check our databases and find out what language is
--- spoken in this country, so we can call in a translator to work with you.
+ -- this country's officially recognized language. Check our databases and find out what language is
+ -- spoken in this country, so we can call in a translator to work with you.
 
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
--- to a different country, a country where people speak only the language she was learning. Find out which
---  nearby country speaks nothing but that language.
+ -- to a different country, a country where people speak only the language she was learning. Find out which
+ --  nearby country speaks nothing but that language.
 
 
 
@@ -24,8 +24,8 @@
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
--- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
--- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+ -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
+ -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 
 
@@ -40,15 +40,15 @@
  -- landing dock. Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
  -- if we can crack it, we can finally put her where she belongs – behind bars.
 
--- Our playdate of late has been unusually fun –
--- As an agent, I'll say, you've been a joy to outrun.
--- And while the food here is great, and the people – so nice!
--- I need a little more sunshine with my slice of life.
--- So I'm off to add one to the population I find
--- In a city of ninety-one thousand and now, eighty five.
+ -- Our playdate of late has been unusually fun –
+ -- As an agent, I'll say, you've been a joy to outrun.
+ -- And while the food here is great, and the people – so nice!
+ -- I need a little more sunshine with my slice of life.
+ -- So I'm off to add one to the population I find
+ -- In a city of ninety-one thousand and now, eighty five.
 
 
--- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+ -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
 
 
